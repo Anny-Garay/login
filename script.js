5b11d30dd5ff5c311c169deedf4d882b6c761aca
Anny-Garay <annyloyz@hotmail.com>
2025-08-08 03:34:46 -0500

    fin:)

--- script.js
+++
@@ -1,6 +0,0 @@
-const grower = document.querySelector('.grower');
-const select = document.querySelector('select');
-
-select.addEventListener('change', ({target}) => {
-  grower.style.animationTimingFunction = target.value;
-});
